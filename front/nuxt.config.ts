6b87818d9d6e7f46762eecb83a8fa0bd5013a941
Wallas Maciel <wallasmaciel919@gmail.com>
2025-03-09 15:47:53 -0300

    feat: set up initial Nuxt frontend with Vuetify
    - Install Nuxt.js
    - Add Vuetify for UI components
    - Configure basic layout

---
+++ front/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["vuetify-nuxt-module"],
+  vuetify: {
+    moduleOptions: {
+      /* module specific options */
+    },
+    vuetifyOptions: {
+      /* vuetify options */
+    },
+  },
+});

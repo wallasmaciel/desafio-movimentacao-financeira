1ded5d02c772a9869d0ef50e91bd13f8384fb154
Wallas Maciel <wallasmaciel919@gmail.com>
2025-03-09 18:09:39 -0300

    feat: configure @vee-validate/nuxt for form validation
    - Install and set up @vee-validate/nuxt
    - Add basic validation rules

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module"],
+  modules: ["vuetify-nuxt-module", "@vee-validate/nuxt"],
   vuetify: {
     moduleOptions: {
       /* module specific options */

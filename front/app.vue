85f8a9bf9c88094467adc83d26a7ce65e4d2516c
Wallas Maciel <wallasmaciel919@gmail.com>
2025-03-09 18:10:15 -0300

    feat: add authenticated user middleware and layout
    - Create auth middleware to protect routes
    - Design and implement authenticated user layout

--- front/app.vue
+++ front/app.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-btn color="primary">Clique aqui</v-btn>
-    </v-container>
-  </v-app>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ front/layouts/authenticated.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card color="grey-lighten-4" style="min-height: 100vh;" rounded="0" flat>
+        <v-toolbar density="compact" height="80">
+            <v-toolbar-title>Movimentos</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn @click="handleToMoviments">
+                <span>Movimentações</span>
+            </v-btn>
+
+            <v-btn @click="handleToCategories">
+                <span>Categorias</span>
+            </v-btn>
+        </v-toolbar>
+        <main class="overflow-auto" style="max-height: calc(100vh - 80);">
+            <slot></slot>
+        </main>
+    </v-card>
+</template>
+
+<script setup>
+    const handleToMoviments = () => navigateTo('/movements')
+    const handleToCategories = () => navigateTo('/categories')
+</script>
